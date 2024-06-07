74864ab58edd83bc2b4730a0ab851438db12c044
kuviman <kuviman@gmail.com>
2024-06-07 16:20:45 +0400

    TupleZip & call_for_tuples_2

--- crates/tuple-macros/src/lib.rs
+++ crates/tuple-macros/src/lib.rs
@@ -29,3 +29,46 @@ macro_rules! call_for_tuples {
         $macro!(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9);
     };
 }
+
+#[macro_export]
+macro_rules! call_for_tuples_2 {
+    ($macro:ident) => {
+        $macro!(;);
+        $macro!(a0; b0);
+        $macro!(a0, a1; b0, b1);
+        $macro!(a0, a1, a2; b0, b1, b2);
+        $macro!(a0, a1, a2, a3; b0, b1, b2, b3);
+        $macro!(a0, a1, a2, a3, a4; b0, b1, b2, b3, b4);
+        $macro!(a0, a1, a2, a3, a4, a5; b0, b1, b2, b3, b4, b5);
+        $macro!(a0, a1, a2, a3, a4, a5, a6; b0, b1, b2, b3, b4, b5, b6);
+        $macro!(a0, a1, a2, a3, a4, a5, a6, a7; b0, b1, b2, b3, b4, b5, b6, b7);
+        $macro!(a0, a1, a2, a3, a4, a5, a6, a7, a8; b0, b1, b2, b3, b4, b5, b6, b7, b8);
+        $macro!(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9; b0, b1, b2, b3, b4, b5, b6, b7, b8, b9);
+    };
+}
+
+pub trait TupleZip<RHS> {
+    type Output;
+    fn tuple_zip(self, rhs: RHS) -> Self::Output;
+}
+
+macro_rules! impl_tuple_zip {
+    ($($a:ident),*; $($b:ident),*) => {
+        impl <$($a,)* $($b,)*> TupleZip<($($b,)*)> for ($($a,)*) {
+            type Output = ($(($a, $b),)*);
+            fn tuple_zip(self, ($($b,)*): ($($b,)*)) -> Self::Output {
+                #![allow(clippy::unused_unit)]
+                let ($($a,)*) = self;
+                ($(($a, $b),)*)
+            }
+        }
+    };
+}
+
+call_for_tuples_2!(impl_tuple_zip);
+
+#[test]
+fn test_tuple_zip() {
+    let zipped = (1, 2).tuple_zip(("hello", "world"));
+    assert_eq!(zipped, ((1, "hello"), (2, "world")));
+}
