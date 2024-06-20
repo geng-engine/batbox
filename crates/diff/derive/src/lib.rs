3024cc714c5bf398d5853ea27e1201a90a9ace9a
kuviman <kuviman@gmail.com>
2024-06-20 22:53:18 +0400

    Diff with darling

--- crates/diff/derive/src/lib.rs
+++ crates/diff/derive/src/lib.rs
@@ -3,141 +3,134 @@
 
 extern crate proc_macro;
 
-use darling::{FromDeriveInput, FromField};
+use darling::{FromDeriveInput, FromField, FromMeta};
 use proc_macro2::{Span, TokenStream};
 use quote::quote;
 
 #[proc_macro_derive(Diff, attributes(diff))]
 pub fn derive_diff(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    derive_impl(input.into()).into()
+    let input: syn::DeriveInput = syn::parse_macro_input!(input);
+    match DeriveInput::from_derive_input(&input) {
+        Ok(input) => input.derive().into(),
+        Err(e) => e.write_errors().into(),
+    }
 }
 
-// TODO: simplify by actually using darling
+#[derive(FromDeriveInput)]
+#[darling(supports(struct_named))]
+struct DeriveInput {
+    ident: syn::Ident,
+    generics: syn::Generics,
+    data: darling::ast::Data<(), Field>,
+}
+
+#[derive(FromField)]
+#[darling(attributes(has_id))]
+struct Field {
+    ident: Option<syn::Ident>,
+    vis: syn::Visibility,
+    ty: syn::Type,
+    #[darling(default)]
+    mode: DiffMode,
+}
+
+#[derive(FromMeta)]
 enum DiffMode {
     Diff,
     Clone,
     Eq,
 }
 
-fn derive_impl(input: TokenStream) -> TokenStream {
-    let s = input.to_string();
-    let ast: syn::DeriveInput = syn::parse_str(&s).unwrap();
-    let input_type = &ast.ident;
-    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
-    let generics = &ast.generics;
-    match ast.data {
-        syn::Data::Struct(syn::DataStruct { ref fields, .. }) => {
-            let field_tys: Vec<_> = fields.iter().map(|field| &field.ty).collect();
-            let field_tys = &field_tys;
-            let field_names: Vec<_> = fields
-                .iter()
-                .map(|field| field.ident.as_ref().unwrap())
-                .collect();
-            let field_names = &field_names;
-            let delta_type = syn::Ident::new(
-                &format!("{input_type}Delta"),
-                proc_macro2::Span::call_site(),
-            );
+impl Default for DiffMode {
+    fn default() -> Self {
+        Self::Diff
+    }
+}
 
-            let field_diff_modes: Vec<DiffMode> = fields
-                .iter()
-                .map(|field| {
-                    let mut diff_type = DiffMode::Diff;
-                    for attr in &field.attrs {
-                        if let syn::Meta::NameValue(syn::MetaNameValue {
-                            path: ref meta_path,
-                            value:
-                                syn::Expr::Lit(syn::ExprLit {
-                                    lit: syn::Lit::Str(ref s),
-                                    ..
-                                }),
-                            ..
-                        }) = attr.meta
-                        {
-                            if meta_path.is_ident("diff") {
-                                diff_type = match s.value().as_str() {
-                                    "eq" => DiffMode::Eq,
-                                    "diff" => DiffMode::Diff,
-                                    "clone" => DiffMode::Clone,
-                                    _ => panic!("Unexpected diff type"),
-                                }
-                            }
-                        }
-                    }
-                    diff_type
-                })
-                .collect();
-
-            let field_diff_types =
-                field_tys
-                    .iter()
-                    .zip(field_diff_modes.iter())
-                    .map(|(field_ty, field_diff_mode)| match field_diff_mode {
-                        DiffMode::Diff => quote! {
-                            <#field_ty as Diff>::Delta
-                        },
-                        DiffMode::Clone => quote! {
-                            #field_ty
-                        },
-                        DiffMode::Eq => quote! {
-                            Option<#field_ty>
-                        },
-                    });
-
-            let field_diffs = field_names.iter().zip(field_diff_modes.iter()).map(
-                |(field_name, field_diff_mode)| match field_diff_mode {
-                    DiffMode::Diff => quote! {
-                        Diff::diff(&self.#field_name, &to.#field_name)
-                    },
-                    DiffMode::Clone => quote! {
-                        to.#field_name.clone()
-                    },
-                    DiffMode::Eq => quote! {
-                        if self.#field_name == to.#field_name {
-                            None
-                        } else {
-                            Some(to.#field_name.clone())
-                        }
-                    },
+impl DeriveInput {
+    fn derive(&self) -> TokenStream {
+        let trait_path = quote! { Diff }; // TODO batbox::Diff or smth?
+        let fields = match &self.data {
+            darling::ast::Data::Struct(fields) => fields,
+            _ => panic!("only structs supported"),
+        };
+        let generics = &self.generics;
+        let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+        let delta_type = syn::Ident::new(
+            &format!("{}Delta", self.ident),
+            proc_macro2::Span::call_site(),
+        );
+        let field_names = fields
+            .iter()
+            .map(|field| field.ident.as_ref().unwrap())
+            .collect::<Vec<_>>();
+        let field_diff_types = fields.iter().map(|field| {
+            let field_ty = &field.ty;
+            match field.mode {
+                DiffMode::Diff => quote! {
+                    <#field_ty as #trait_path>::Delta
                 },
-            );
-
-            let field_updates = field_names.iter().zip(field_diff_modes.iter()).map(
-                |(field_name, field_diff_mode)| match field_diff_mode {
-                    DiffMode::Diff => quote! {
-                        Diff::update(&mut self.#field_name, &delta.#field_name);
-                    },
-                    DiffMode::Clone => quote! {
-                        self.#field_name = delta.#field_name.clone();
-                    },
-                    DiffMode::Eq => quote! {
-                        if let Some(value) = &delta.#field_name {
-                            self.#field_name = value.clone();
-                        }
-                    },
+                DiffMode::Clone => quote! {
+                    #field_ty
                 },
-            );
-
-            let expanded = quote! {
-                #[derive(Debug, Serialize, Deserialize, Clone)]
-                pub struct #delta_type #generics {
-                    #(#field_names: #field_diff_types,)*
-                }
-
-                impl #impl_generics Diff for #input_type #ty_generics #where_clause {
-                    type Delta = #delta_type;
-                    fn diff(&self, to: &Self) -> Self::Delta {
-                        #delta_type {
-                            #(#field_names: #field_diffs,)*
-                        }
+                DiffMode::Eq => quote! {
+                    Option<#field_ty>
+                },
+            }
+        });
+        let field_diffs = fields.iter().map(|field| {
+            let field_name = &field.ident;
+            match field.mode {
+                DiffMode::Diff => quote! {
+                    #trait_path::diff(&self.#field_name, &to.#field_name)
+                },
+                DiffMode::Clone => quote! {
+                    to.#field_name.clone()
+                },
+                DiffMode::Eq => quote! {
+                    if self.#field_name == to.#field_name {
+                        None
+                    } else {
+                        Some(to.#field_name.clone())
                     }
-                    fn update(&mut self, delta: &Self::Delta) {
-                        #(#field_updates)*
+                },
+            }
+        });
+
+        let field_updates = fields.iter().map(|field| {
+            let field_name = &field.ident;
+            match field.mode {
+                DiffMode::Diff => quote! {
+                    Diff::update(&mut self.#field_name, &delta.#field_name);
+                },
+                DiffMode::Clone => quote! {
+                    self.#field_name = delta.#field_name.clone();
+                },
+                DiffMode::Eq => quote! {
+                    if let Some(value) = &delta.#field_name {
+                        self.#field_name = value.clone();
+                    }
+                },
+            }
+        });
+        let input_type = &self.ident;
+        quote! {
+            #[derive(Debug, Serialize, Deserialize, Clone)]
+            pub struct #delta_type #generics {
+                #(#field_names: #field_diff_types,)*
+            }
+
+            impl #impl_generics Diff for #input_type #ty_generics #where_clause {
+                type Delta = #delta_type;
+                fn diff(&self, to: &Self) -> Self::Delta {
+                    #delta_type {
+                        #(#field_names: #field_diffs,)*
                     }
                 }
-            };
-            expanded
+                fn update(&mut self, delta: &Self::Delta) {
+                    #(#field_updates)*
+                }
+            }
         }
-        _ => panic!("Diff can only be derived by structs"),
     }
 }
